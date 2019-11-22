2dae49a6fc45cd22e9f4ee3ee2432f03008c31e1
David Mendoza <dmdmendoza719@gmail.com>
2019-11-22 12:54:52 -0600

    Adds methods from other projects

--- src/linux/final-project/src/main.c
+++ src/linux/final-project/src/main.c
@@ -12,6 +12,106 @@
 #include <unistd.h>
 #include "superblock.h"
 
+#define DIR_REC_LEN(DirEntry) (ceil((float)(DirEntry.name_len + \
+                            sizeof(UINT8)) / 4) * 4); 
+
+void sbGetPrimarySuperblock(int pFile)
+{
+	// printf("GET SUPERBLOCK\n");
+	tSuperblock primarySB;
+
+	if (lseek(pFile, SB_OFFSET, SEEK_SET) < 0)
+	{
+		perror("lseek() error");
+		exit(1);
+	}
+
+	if (read(pFile, &primarySB, sizeof(tSuperblock)) < 0)
+	{
+		perror("read() error");
+		exit(1);
+	}
+
+	gPrimarySuperblock = primarySB;
+}
+
+INT4 InodeDirReadRecord(CHAR *pEntries, UINT4 u4StartPos, 
+        struct ext3_dir_entry_2 *pDirEntry)
+{
+    CHAR *pPos = NULL;
+
+    if (pEntries == NULL)
+    {
+        return -1;
+    }
+
+    pPos = pEntries + u4StartPos;
+    memcpy(pDirEntry, pPos, sizeof(UINT8));
+    strncpy(pDirEntry->name, pPos + sizeof(UINT8),
+            pDirEntry->name_len);
+    return 1;
+}
+
+/*
+This utility function compares the hex values of two character arrays upto a certain length (n)
+*/
+int compareHexValues(unsigned char string1[], unsigned char string2[], int n)
+{
+	int i;
+	for (i = 0; i < n; i++)
+	{
+		if ((int)string1[i] == (int)string2[i])
+		{
+			if (i == n - 1)
+			{
+				return 1;
+			}
+		}
+		else
+		{
+			return 0;
+		}
+	}
+	return 0;
+}
+
+void insertInode(int fp, int block, int inode)
+{
+	unsigned int block_size = 1024 << gPrimarySuperblock.s_log_block_size;
+	lseek(fp, block * block_size, SEEK_SET);
+	unsigned char buf[block_size];
+	read(fp, buf, block_size);
+	// go through all the current records
+	struct ext3_dir_entry_2 DirEntry;
+	unsigned int nextRecord = 0;
+	unsigned int readSoFar = 0;
+	while (1)
+	{
+		memset(&DirEntry, 0, sizeof(DirEntry));
+		InodeDirReadRecord(buf, nextRecord, &DirEntry);
+		nextRecord += DirEntry.rec_len;
+		// when we find the last entry, exit the loop
+		if (nextRecord == block_size)
+			break;
+		else
+			readSoFar += DirEntry.rec_len;
+	}
+	DirEntry.rec_len = DIR_REC_LEN(DirEntry);
+	memcpy(buf + readSoFar, &DirEntry, DirEntry.rec_len);
+	
+	struct ext3_dir_entry_2 NewEntry;
+	memset(&NewEntry, 0, sizeof(NewEntry));
+	NewEntry.inode = inode;
+	NewEntry.rec_len = block_size - (readSoFar + DirEntry.rec_len);
+	NewEntry.file_type = 1;
+	char* name = "zfile.gif";
+	strcpy(NewEntry.name, name);
+	NewEntry.name_len = 9;
+	memcpy(buf + readSoFar + DirEntry.rec_len, &NewEntry, NewEntry.rec_len);
+	lseek(fp, block * block_size, SEEK_SET);
+	write(fp, buf, block_size);
+}
+
 //Converts 4 byte hexadecimal to decimal
 long long hexToNum(unsigned char hex[]){
 	return ((long long) hex[3] << 24) | (hex[2] << 16) | (hex[1] << 8) | hex[0];
